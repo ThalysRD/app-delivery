461ebc12c69b449ac936706a40eac90140dd836b
Gabriel Grippa Alves <gabrielgrippa1@gmail.com>
2022-10-27 20:48:44 -0300

    Cria funções necessárias login no backend

--- back-end/src/database/models/sale.js
+++ back-end/src/database/models/sale.js
@@ -1,30 +1,29 @@
-const createSale = (sequelize, DataTypes) => {
-    const Sale = sequelize.define('sales', {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement:true,
-        allowNull: false,
-      } ,
-      user_id:: DataTypes.INTEGER,
-      seller_id: DataTypes.INTEGER,
-      total_price: DataTypes.INTEGER,
-      delivery_address: DataTypes.STRING,
-      delivery_number: DataTypes.STRING,
-      sale_date: DataTypes.DATE,
-      status: DataTypes.STRING
-    }, {
-      tableName: 'sales',
-      timestamps: false,
-    });
+const Sale = (sequelize, DataTypes) => {
+  const Sale = sequelize.define('Sale', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement:true,
+      allowNull: false,
+    },
+    user_id: DataTypes.INTEGER,
+    seller_id: DataTypes.INTEGER,
+    total_price: DataTypes.INTEGER,
+    delivery_address: DataTypes.STRING,
+    delivery_number: DataTypes.STRING,
+    sale_date: DataTypes.DATE,
+    status: DataTypes.STRING
+  }, {
+    tableName: 'sales',
+    timestamps: false,
+  });
 
-    Sale.belongsTo(User, { foreignKey: 'user_id' });
-
-    Sale.belongsTo(User, { foreignKey: 'seller_id' });
-  
-  
-  return Sale
-  
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.User, { foreignKey: 'user_id' });  
+    Sale.belongsTo(models.User, { foreignKey: 'seller_id' });
   }
-  
-  module.exports = createSale
+
+  return Sale;
+};
+
+module.exports = Sale
