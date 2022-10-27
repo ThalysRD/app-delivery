2d1b8fb72f011a9d41b46ca776b88d1fc248bfa0
ThalysRD <tgbr66@gmail.com>
2022-10-27 18:32:31 -0300

    Cria os inputs password e login para a página de login

--- front-end/src/components/LoginCard.jsx
+++ front-end/src/components/LoginCard.jsx
@@ -3,9 +3,26 @@ import React, { Component } from 'react';
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <section>
+        <div>
+          <label htmlFor="input-email">
+            Email
+            <input
+              id="input-email"
+              type="email"
+              data-testid="common_login__input-email"
+            />
+          </label>
+          <label htmlFor="input-password">
+            Password
+            <input
+              id="input-password"
+              type="password"
+              data-testid="common_login__input-password"
+            />
+          </label>
+        </div>
+      </section>
     );
   }
 }
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import LoginCard from '../components/LoginCard';
 
 export default class Login extends Component {
   render() {
     return (
-      <div>Login</div>
+      <section><LoginCard /></section>
     );
   }
 }
