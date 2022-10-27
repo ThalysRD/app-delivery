939566c6da940f0abaa5f1c58ac03d1df89375fe
ThiagoGlady <thiagoglady@gmail.com>
2022-10-27 19:24:51 -0300

    (req 2) Feito o componente do Login

--- front-end/src/components/LoginCard.jsx
+++ front-end/src/components/LoginCard.jsx
@@ -5,28 +5,62 @@ export default class LoginCard extends Component {
     super(props);
 
     console.log('constructor');
+
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
 
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
+  };
+
   render() {
+    const { email, password } = this.state;
+
     return (
       <section>
         <div>
           <label htmlFor="input-email">
-            Email
+            Email:
             <input
               id="input-email"
               type="email"
               data-testid="common_login__input-email"
+              onChange={ this.handleChange }
+              name="email"
+              value={ email }
             />
           </label>
+          <br />
           <label htmlFor="input-password">
-            Password
+            Password:
             <input
               id="input-password"
               type="password"
               data-testid="common_login__input-password"
+              onChange={ this.handleChange }
+              name="password"
+              value={ password }
             />
           </label>
+        </div>
+        <div>
+          <button
+            data-testid="common_login__button-login"
+            type="button"
+          >
+            Login
+          </button>
+          <button
+            data-testid="common_login__button-register"
+            type="button"
+          >
+            Register
+          </button>
         </div>
       </section>
     );
