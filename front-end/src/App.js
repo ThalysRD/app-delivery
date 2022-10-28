af393600cc0cee8826a39edf863a8637d933fd5b
ThalysRD <tgbr66@gmail.com>
2022-10-28 18:13:53 -0300

    Adiciona ao app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/login" component={ Login } />
       <Route exact path="/" component={ Home } />
+      <Route exact path="/" component={ Register } />
     </Switch>
   );
 }
