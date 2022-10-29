668f8934659c4f7ce49e6fd2a8d189bc21f68be0
ThiagoGlady <thiagoglady@gmail.com>
2022-10-29 10:08:06 -0300

    refactor: :recycle: Mudança no serviço de login. Agora ele retorna também o email e o nome para o localStorage após o login do usuário.

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('../routes');
-var cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/login', cors(), routes.loginRoute)
+app.use('/login', cors(), routes.loginRoute);
 app.use('/register', cors(), routes.userRoute);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
