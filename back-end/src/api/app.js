baf6cb46e3e79ad2cfb0c3afdacfa915451ef6b5
ThalysRD <tgbr66@gmail.com>
2022-10-31 19:47:01 -0300

    Correção do arquivo app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
+var cors = require('cors');
 const routes = require('../routes');
-var cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
 
 app.use(express.static('public'));
-app.use('/login', cors(), routes.loginRoute)
+app.use('/login', cors(), routes.loginRoute);
 app.use('/register', cors(), routes.userRoute);
 app.use('/products', cors(), routes.productRoute);
 
