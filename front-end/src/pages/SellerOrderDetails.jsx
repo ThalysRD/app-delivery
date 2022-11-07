6213c6b69df8f714797ee113542642bdee983066
ThiagoGlady <thiagoglady@gmail.com>
2022-11-07 16:40:38 -0300

    fazendo pull da base

--- front-end/src/pages/SellerOrderDetails.jsx
+++ front-end/src/pages/SellerOrderDetails.jsx
@@ -4,12 +4,36 @@ import NavBar from '../components/NavBar';
 import SellerDetails from '../components/SellerDetails';
 
 export default class SellerOrderDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      orderProducts: [],
+      orderDetails: {},
+    };
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const { orderDetails, orderProducts } = await getOrderDetails(match.params.id);
+
+    this.setState({
+      orderProducts,
+      orderDetails,
+    });
+  }
+
   render() {
     const { history } = this.props;
+    const { orderProducts, orderDetails } = this.state;
+
     return (
       <section>
         <NavBar history={ history } />
-        <SellerDetails />
+        <SellerDetails
+          orderProducts={ orderProducts }
+          orderDetails={ orderDetails }
+        />
       </section>
     );
   }
@@ -19,6 +43,12 @@ SellerOrderDetails.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }),
+
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
 };
 
 SellerOrderDetails.defaultProps = {
